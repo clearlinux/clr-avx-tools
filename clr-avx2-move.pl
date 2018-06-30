b48a8636cf920506b36e4c84d11e526fcc04eb8d
Thiago Macieira <thiago.macieira@intel.com>
2018-06-29 17:53:41 -0700

    Perform the directory scanning in clr-avs2-move.pl itself
    Simplifies macros in the .spec files.
    
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- clr-avx2-move.pl
+++ clr-avx2-move.pl
@@ -1,51 +1,100 @@
 #!/usr/bin/perl
 use strict;
-if (scalar @ARGV < 2) {
-    print "clr-avx2-move.pl <libsubdir> <pluginsuffix> [file listing]\n";
+if (scalar @ARGV < 3) {
+    print "clr-avx2-move.pl <libsubdir> <pluginsuffix> <buildroot>\n";
     print "Examples:\n";
-    print "  clr-avx2-move.pl haswell .avx2 /usr/lib64/*.so*\n";
-    print "  clr-avx2-move.pl haswell/avx512_1 .avx512 /usr/lib64/*.so*\n";
+    print "  clr-avx2-move.pl haswell .avx2 /var/tmp/buildroot\n";
+    print "  clr-avx2-move.pl haswell/avx512_1 .avx512 /var/tmp/buildroot\n";
     exit 0;
 }
 
 my $libsubdir = shift @ARGV;
 my $pluginsuffix = shift @ARGV;
+my $root = shift @ARGV;
 my @files;
 my %createddirs;
 
-# Sort the incoming file listing
-for my $f (@ARGV) {
-    # Ignore if it's not a file
-    next unless (-f $f);
-    # Put symlinks at the beginning of the list
-    # (to avoid breaking them when we move the actual file)
-    if (-l $f) {
-        unshift @files, $f;
-    } else {
-        push @files, $f;
+sub scandir($$$) {
+    my $dir = $_[0];
+    my $needs_exec = $_[1];
+    my $name_match = $_[2];
+
+    # Only recurse into this dir if it exists and is not a symlink
+    return unless (-d $dir && ! -l $dir);
+
+    opendir(my $dh, $dir) or die "Couldn't open $dir: $!";
+    while (readdir $dh) {
+        next if $_ eq "." || $_ eq "..";
+
+        my $f = "$dir/$_";
+        if (-f $f) {
+            next unless (-x $f || !$needs_exec);
+            next unless m/$name_match/;
+
+            # Put symlinks at the beginning of the list
+            # (to avoid breaking them when we move the actual file)
+            if (-l $f) {
+                unshift @files, $f;
+            } else {
+                push @files, $f;
+            }
+        } elsif (-d $f) {
+            scandir($f);
+        }
     }
+    closedir $dh;
 }
 
+# Build the file listing
+scandir("$root/bin", 1, qr//);
+scandir("$root/sbin", 1, qr//);
+scandir("$root/usr/bin", 1, qr//);
+scandir("$root/usr/sbin", 1, qr//);
+scandir("$root/usr/local/bin", 1, qr//);
+scandir("$root/usr/local/sbin", 1, qr//);
+scandir("$root/usr/lib64", 0, qr/\.so\.?/);
+
+# Save STDERR for us, but redirect it to /dev/null for readelf
+open(REAL_STDERR, ">&STDERR");
+open(STDERR, ">/dev/null");
+
+# Automatically flush STDOUT
+$| = 1;
+
+# Make sure readelf outputs in English
+$ENV{LC_ALL} = 'C';
+
 for my $f (@files) {
-    # Run readelf -d on the file to get the SONAME
-    open READELF, "-|", "readelf", "-d", $f;
-    my @soname = grep / *0x\w+ \(SONAME\)\s/, <READELF>;
+    my $soname = 0;
+    my $interpreter = 0;
+    my $elftype;
+
+    # Run readelf -hdl on the file to get the SONAME and INTERP
+    open READELF, "-|", "readelf", "-hdl", $f;
+    while (<READELF>) {
+        $elftype = $1 if /^\s*Type:\s*(\w+)\b/;
+        $soname = 1 if / *0x\w+ \(SONAME\)\s/;
+        $interpreter = 1 if /^\s*INTERP\s/;
+    }
     close READELF;
 
-    next if $? >> 8;  # not ELF, ignore (could be a linker script)
+    next if $? >> 8;  # not ELF, ignore (could be a script)
+    next unless $elftype eq "EXEC" || $elftype eq "DYN";
 
     my $to;
-    if (scalar @soname) {
-        # This ELF module has a SONAME, so it's a library:
-        # Move it to the $libsubdir subdir
+    if ($soname || $interpreter) {
+        # This ELF file either has a SONAME (it's a library), an interpreter
+        # (it's an executable), or both (it's an executable library).
+        # Move it to the $libsubdir subdir.
         $f =~ m,^(.*?)/?([^/]+)$,;
         my $dirname = "$1/$libsubdir";
         $to = "$dirname/$2";
         mkdir($dirname) unless defined($createddirs{$dirname});
         $createddirs{$dirname} = 1;
     } else {
+        # No SONAME or interpreter, it must be a plugin.
         $to = $f . $pluginsuffix;
     }
     rename($f, $to) and print "$f -> $to\n"
-        or print STDERR "rename(\"$f\", \"$to\"): $!\n";
+        or print REAL_STDERR "rename(\"$f\", \"$to\"): $!\n";
 }
